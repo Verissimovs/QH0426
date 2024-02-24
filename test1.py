31d73d5fca94b17470f52dc753b706ff1ffb3f45
veris <verissimovs@gmail.com>
2024-02-24 15:44:20 +0000

    test1

--- test1.py
+++ test1.py
@@ -1,2 +1,9 @@
 print("*_*")
 print("jesus esta voltando")
+print("vem logo pai,estou te esperando")
+
+# display message to the screen
+
+# display message to the screen
+print("\tbusque a presenca do pai")
+print("enquanto voce pode encontra-lo")
