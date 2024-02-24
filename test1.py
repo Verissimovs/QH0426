54f9834096bbd26ae5f4b04cff9b3c4e719cb69b
veris <verissimovs@gmail.com>
2024-02-24 16:02:13 +0000

    test1

--- test1.py
+++ test1.py
@@ -7,3 +7,7 @@ print("vem logo pai,estou te esperando")
 # display message to the screen
 print("\tbusque a presenca do pai")
 print("enquanto voce pode encontra-lo")
+#ask user to enter the name
+print("what is your name?")
+name = input()
+print(f"it is nice to meet you{name}")
